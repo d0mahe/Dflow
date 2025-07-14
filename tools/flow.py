b549ee42648ef750e1ac382a3276ecd9fac1728b
ZPyAo <121719459+LilYau350@users.noreply.github.com>
2025-07-15 03:41:12 +0800

    modify

--- tools/flow.py
+++ tools/flow.py
@@ -18,94 +18,53 @@ from einops import rearrange
 from functools import partial
 
 
-
-class ModelMeanType(enum.Enum):
-    """
-    Which type of output the model predicts.
-    """
-    PREVIOUS_X = enum.auto()  # the model predicts x_{t-1}
-    START_X = enum.auto()  # the model predicts x_0
-    EPSILON = enum.auto()  # the model predicts epsilon
-    VELOCITY = enum.auto() # the model predicts velocity alpha_t * epsilon - sigma_t * x_0
-    VECTOR = enum.auto() # the model predicts v in flow matching d_sigma_t * epsilon - d_alpha_t * x_0
-    SCORE = enum.auto()  # the model predicts the score function
     
-
-# def _extract_into_tensor(arr, timesteps, broadcast_shape):
-#     """
-#     Extract values from a 1-D numpy array for a batch of indices.
-
-#     :param arr: the 1-D numpy array.
-#     :param timesteps: a tensor of indices into the array to extract.
-#     :param broadcast_shape: a larger shape of K dimensions with the batch
-#                             dimension equal to the length of timesteps.
-#     :return: a tensor of shape [batch_size, 1, ...] where the shape has K dims.
-#     """
-#     res = torch.from_numpy(arr).to(device=timesteps.device)[timesteps].float()
-#     while len(res.shape) < len(broadcast_shape):
-#         res = res[..., None]
-#     return res.expand(broadcast_shape)
-
-# utils
-@torch.no_grad()
-def concat_all_gather(tensor):
-    """
-    Performs all_gather operation on the provided tensors.
-    *** Warning ***: torch.distributed.all_gather has no gradient.
-    """
-    if not dist.is_initialized():
-        return output
-
-    tensors_gather = [torch.ones_like(tensor)
-        for _ in range(torch.distributed.get_world_size())]
-    torch.distributed.all_gather(tensors_gather, tensor, async_op=False)
-
-    output = torch.cat(tensors_gather, dim=0)
-    return output
-
 def stopgrad(x):
     return x.detach()
 
-def adaptive_l2_loss(error, gamma=0.5, c=1e-3):
-    """
-    Adaptive L2 loss: sg(w) * ||Δ||_2^2, where w = 1 / (||Δ||^2 + c)^p, p = 1 - γ
-    Args:
-        error: Tensor of shape (B, C, W, H)
-        gamma: Power used in original ||Δ||^{2γ} loss
-        c: Small constant for stability
-    Returns:
-        Scalar loss
-    """
-    delta_sq = torch.mean(error ** 2, dim=(1, 2, 3), keepdim=False)
-    p = 1.0 - gamma
-    w = 1.0 / (delta_sq + c).pow(p)
-    loss = delta_sq  # ||Δ||^2
-    return (stopgrad(w) * loss).mean()
 
 class FlowMatching:
     def __init__(
         self,
         *,
         model_mean_type,
-        mse_loss_weight_type='constant',
         path_type,         
+        flow_ratio=0.50, #control how many time steps satisfy r=t, when flow ratio = 1.0,  mean flow degrades as flow matching
+        time_dist=['lognorm', -0.4, 1.0], #sampling timestep interval [r, t].
+        mf_cfg_ratio=0.10,
+        # set scale as none to disable CFG distill
+        mf_cfg_scale=2.0,
+        # experimental
+        mf_cfg_uncond='v',
+        jvp_api='autograd',
+        
         sampler_type="sde",   
-        p2_gamma=1,
-        p2_k=1,
         atol=1e-6,
         rtol=1e-5,
     ):
+        # flow matching settings
         self.path_type = path_type        
         self.model_mean_type = model_mean_type
-        self.mse_loss_weight_type = mse_loss_weight_type
-        self.sampler_type = sampler_type
-        # P2 weighting
-        self.p2_gamma = p2_gamma
-        self.p2_k = p2_k
-        #sample tolenace
+        # mean flow settings
+        self.flow_ratio = flow_ratio
+        self.time_dist = time_dist
+        self.mf_cfg_ratio = mf_cfg_ratio
+        self.mf_cfg_uncond = mf_cfg_uncond        
+        self.w = mf_cfg_scale
+        self.jvp_api = jvp_api
+        assert jvp_api in ['funtorch', 'autograd'], "jvp_api must be 'funtorch' or 'autograd'"
+        if jvp_api == 'funtorch':
+            self.jvp_fn = torch.func.jvp
+            self.create_graph = False
+        elif jvp_api == 'autograd':
+            self.jvp_fn = torch.autograd.functional.jvp
+            self.create_graph = True
+        
+        #sample settings
         self.atol = atol
         self.rtol = rtol
-        
+        self.sampler_type = sampler_type
+                    
     def expand_t_like_x(self, t, x):
         if t.dim() == 0:
             t = t.expand(x.shape[0])
@@ -131,58 +90,72 @@ class FlowMatching:
 
         return alpha_t, sigma_t, d_alpha_t, d_sigma_t
 
-    def convert_model_output_to_vector(self, model_output, x_t, t):
-        alpha_t, sigma_t, d_alpha_t, d_sigma_t = self.interpolant(t)
+    # fix: r should be always not larger than t
+    def sample_t_r(self, x_start):
+        if self.time_dist[0] == 'uniform':
+            samples = np.random.rand(x_start.shape[0], 2).astype(np.float32)
 
-        if self.model_mean_type == ModelMeanType.START_X:
-            start_x = model_output
-            noise = (x_t - alpha_t * start_x) / sigma_t
-            
-        elif self.model_mean_type == ModelMeanType.EPSILON:
-            noise = model_output
-            start_x = (x_t - sigma_t * noise) / alpha_t
-            
-        elif self.model_mean_type == ModelMeanType.VELOCITY:
-            # v = α_t * ε - σ_t * x₀ → solve ε and x₀
-            start_x = (alpha_t * x_t - sigma_t * model_output) / (alpha_t**2 + sigma_t**2)
-            noise = (sigma_t * x_t + alpha_t * model_output) / (alpha_t**2 + sigma_t**2)
-            
-        elif self.model_mean_type == ModelMeanType.VECTOR:
-            return model_output
-        
+        elif self.time_dist[0] == 'lognorm':
+            mu, sigma = self.time_dist[-2], self.time_dist[-1]
+            normal_samples = np.random.randn(x_start.shape[0], 2).astype(np.float32) * sigma + mu
+            samples = 1 / (1 + np.exp(-normal_samples))  # Apply sigmoid, ensure samples are in [0, 1]
+
+        # Assign t = max, r = min, for each pair
+        t_np = np.maximum(samples[:, 0], samples[:, 1])
+        r_np = np.minimum(samples[:, 0], samples[:, 1])
+
+        num_selected = int(self.flow_ratio * x_start.shape[0])
+        indices = np.random.permutation(x_start.shape[0])[:num_selected]
+        r_np[indices] = t_np[indices]
+
+        t = torch.tensor(t_np, device=x_start.device)
+        r = torch.tensor(r_np, device=x_start.device)
+        return t, r
+
+    
+    def apply_cfg_for_mf(self, model, x_t, t, v_hat, c):
+        """
+        Apply classifier-free guidance and return modified v_hat.
+        """
+        if c is None or self.mf_cfg_ratio is None:
+            return v_hat
+
+        uncond = torch.ones_like(c) * getattr(self, "num_classes", 1000)
+        cfg_mask = torch.rand_like(c.float()) < self.mf_cfg_ratio
+        c_cfg = torch.where(cfg_mask, uncond, c)
+
+        if self.w is not None:
+            with torch.no_grad():
+                u_t = model(x_t, t, t, c_cfg)
+            v_hat_guided = self.w * v_hat + (1 - self.w) * u_t
+
+            if self.mf_cfg_uncond == 'v':
+                cfg_mask = rearrange(cfg_mask, "b -> b 1 1 1").bool()
+                v_hat_guided = torch.where(cfg_mask, v_hat, v_hat_guided)
+            return v_hat_guided
         else:
-            raise NotImplementedError("Unsupported model_mean_type for vector")
-
-        vector = d_alpha_t * start_x + d_sigma_t * noise
-        return vector
-
-    def convert_model_output_to_score(self, model_output, x_t, t):
-        alpha_t, sigma_t, d_alpha_t, d_sigma_t = self.interpolant(t)
-
-        if self.model_mean_type == ModelMeanType.START_X:
-            start_x = model_output
-            score = -(x_t - alpha_t * start_x) / (sigma_t ** 2)
-
-        elif self.model_mean_type == ModelMeanType.EPSILON:
-            noise = model_output
-            score = -noise / sigma_t
-
-        elif self.model_mean_type == ModelMeanType.VELOCITY:
-            # v = α_t * ε - σ_t * x₀ → solve ε and x₀
-            noise = (sigma_t * x_t + alpha_t * model_output) / (alpha_t**2 + sigma_t**2)
-            score = -noise / sigma_t
-
-        elif self.model_mean_type == ModelMeanType.VECTOR:
-            # start_x = (sigma_t * model_output - d_sigma_t * x_t) / (sigma_t * d_alpha_t - alpha_t * d_sigma_t)
-            noise = (d_alpha_t * x_t - alpha_t * model_output) / (sigma_t * d_alpha_t - alpha_t * d_sigma_t)
-            score = -noise / sigma_t
-        elif self.model_mean_type == ModelMeanType.SCORE:
-            return model_output
+            return v_hat
         
-        else:
-            raise NotImplementedError("Unsupported model_mean_type for score")
+    def compute_u_target(self, model, x_t, t, r, v_hat, c):
+        """
+        Compute model output and u_target = v_hat - (t - r) * ∂u/∂t
+        """
+        v_hat = self.apply_cfg_for_mf(model, x_t, t, v_hat, c)
+        model_partial = partial(model, y=c) if c is not None else model
 
-        return score
+        jvp_args = (
+            lambda z, t, r: model_partial(z, t, r),
+            (x_t, t, r),
+            (v_hat, torch.ones_like(t), torch.zeros_like(r)),
+        )
+
+        u, dudt = self.jvp_fn(*jvp_args, create_graph=self.create_graph)
+        t_ = self.expand_t_like_x(t, x_t)
+        r_ = self.expand_t_like_x(r, x_t)
+        u_target = v_hat - (t_ - r_) * dudt
+        return u, u_target
+
+
     
     def q_sample(self, x_start, noise, t,):
         t = self.expand_t_like_x(t, x_start)
@@ -191,39 +164,59 @@ class FlowMatching:
         return x_t
     
     #taining
-    def training_losses(self, model, x_start, t=None, model_kwargs=None, noise=None):
+    def training_losses(self, model, x_start, model_kwargs=None, noise=None):
         if model_kwargs is None:
             model_kwargs = {}
         if noise is None:
             noise = torch.randn_like(x_start)
-        if t is None:
-            t = torch.rand(x_start.shape[0], device=x_start.device)
-        
-        alpha_t, sigma_t, d_alpha_t, d_sigma_t = self.interpolant(t)     
-                     
+            
+        t, r = self.sample_t_r(x_start)
+        if self.flow_ratio < 1.0:
+            model_kwargs["r"] = r
+            
         x_t = self.q_sample(x_start, noise, t)
+        _, _, d_alpha_t, d_sigma_t = self.interpolant(t)      
+
+        velocity = d_alpha_t[:, None, None, None] * x_start + d_sigma_t[:, None, None, None] * noise
         
+        y = model_kwargs.get("y", None)
+        
+        mse_loss_weight = torch.ones_like(t)
+
         terms = {}
-        
-        target = {
-            ModelMeanType.START_X: x_start,
-            ModelMeanType.EPSILON: noise,
-            ModelMeanType.VELOCITY: alpha_t[:, None, None, None] * noise - sigma_t[:, None, None, None] * x_start,
-            ModelMeanType.VECTOR: d_alpha_t[:, None, None, None] * x_start + d_sigma_t[:, None, None, None] * noise,
-            ModelMeanType.SCORE: - noise / sigma_t[:, None, None, None],
-            # ModelMeanType.UNRAVEL: unravel,
-        }[self.model_mean_type]
-        
-        model_output = model(x_t, t, **model_kwargs)
-        
-        assert model_output.shape == target.shape == x_start.shape
+        # compute the model output and target according to flow ratio
+        if self.flow_ratio < 1.0:
+            u, u_target = self.compute_u_target(model, x_t, t, r, velocity, c=y)
+            model_output = u            
+            target = u_target.detach()
+        else:
+            model_output = model(x_t, t, **model_kwargs)
+            target = velocity
 
         raw_mse = mean_flat((target - model_output) ** 2)
-            
-        terms["loss"] = raw_mse
+        
+        if self.flow_ratio < 1.0:
+            # if flow ratio is less than 1.0, we compute adaptive weight
+            mse_loss_weight = self.adaptive_weight(raw_mse.detach())
+        
+        terms["loss"] = mse_loss_weight * raw_mse
 
         return terms
 
+    def adaptive_weight(self, raw_mse, gamma=0.5, c=1e-3):
+        """
+        Adaptive L2 loss: sg(w) * ||Δ||_2^2, where w = 1 / (||Δ||^2 + c)^p, p = 1 - γ
+        Args:
+            error: Tensor of shape (B, C, W, H)
+            gamma: Power used in original ||Δ||^{2γ} loss
+            c: Small constant for stability
+        Returns:
+            Scalar loss
+        """
+        p = 1.0 - gamma
+        w = 1.0 / (raw_mse + c).pow(p)
+        return w
+    
     #sampling
     def forward_with_cfg(self, model, x, t_in, guidance_scale, **model_kwargs):
         t = t_in.view(x.shape[0]) # make sure the shape fo t inputs mdoel is [batch_dim]
@@ -242,7 +235,8 @@ class FlowMatching:
         def guided_drift(t, x):
             t_in = self.expand_t_like_x(t, x)
             model_output = self.forward_with_cfg(model, x, t_in, guidance_scale, **model_kwargs)
-            return self.convert_model_output_to_vector(model_output, x, t_in)
+            # return self.convert_model_output_to_vector(model_output, x, t_in)
+            return model_output
         
         samples = odeint(
             func=guided_drift,
@@ -257,7 +251,14 @@ class FlowMatching:
     def compute_diffusion(self, t_cur):
         # return self.interpolant(t_cur)[1]
         return self.interpolant(t_cur)[1] ** 2
-    
+
+    def convert_velocity_to_score(self, model_output, x_t, t):
+        alpha_t, sigma_t, d_alpha_t, d_sigma_t = self.interpolant(t)
+        # start_x = (sigma_t * model_output - d_sigma_t * x_t) / (sigma_t * d_alpha_t - alpha_t * d_sigma_t)
+        noise = (d_alpha_t * x_t - alpha_t * model_output) / (sigma_t * d_alpha_t - alpha_t * d_sigma_t)
+        score = -noise / sigma_t
+        return score
+        
     def sde_sample(self, model, noise, device, num_steps=50, solver='heun', guidance_scale=1.0, **model_kwargs):
         """
         SDE sampler with Euler or Heun method and final deterministic step.
@@ -265,8 +266,8 @@ class FlowMatching:
         """
         def compute_drift(x, t_tensor, diffusion):
             out = self.forward_with_cfg(model, x, t_tensor, guidance_scale, **model_kwargs)
-            s = self.convert_model_output_to_score(out, x, t_tensor)
-            v = self.convert_model_output_to_vector(out, x, t_tensor)
+            s = self.convert_velocity_to_score(out, x, t_tensor)
+            v = out
             return v - 0.5 * diffusion * s
 
         t_steps = torch.linspace(1.0, 0.04, num_steps, dtype=torch.float64, device=device)
@@ -317,14 +318,26 @@ class FlowMatching:
             raise NotImplementedError(f"Unsupported sampler_type: {self.sampler_type}")
 
 
-
-
+def adaptive_l2_loss(error, gamma=0.5, c=1e-3):
+    """
+    Adaptive L2 loss: sg(w) * ||Δ||_2^2, where w = 1 / (||Δ||^2 + c)^p, p = 1 - γ
+    Args:
+        error: Tensor of shape (B, C, W, H)
+        gamma: Power used in original ||Δ||^{2γ} loss
+        c: Small constant for stability
+    Returns:
+        Scalar loss
+    """
+    delta_sq = torch.mean(error ** 2, dim=(1, 2, 3), keepdim=False)
+    p = 1.0 - gamma
+    w = 1.0 / (delta_sq + c).pow(p)
+    loss = delta_sq  # ||Δ||^2
+    return (stopgrad(w) * loss).mean()
 
 
 class MeanFlow:
     def __init__(
         self,
-
         # mean flow settings
         flow_ratio=0.50,
         # time distribution, mu, sigma
